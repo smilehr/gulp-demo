15174387705f63a45a1a92e1bcc29b53352a83ad
smilehr <1064431893@qq.com>
2019-05-19 22:39:06 +0800

    typescript

--- README.md
+++ README.md
@@ -137,4 +137,68 @@ something to how to build one gulp development
       });
       ```
 
-    2. 使用`Browserify`
+    2. 使用`Browserify` 编译ts
+
+      ```js
+      npm install --save-dev browserify tsify vinyl-source-stream
+
+      gulp.task("default", ["copy-html"], function () {
+          return browserify({
+              basedir: '.',
+              debug: true,
+              entries: ['src/main.ts'],
+              cache: {},
+              packageCache: {}
+          })
+          .plugin(tsify)
+          .bundle()
+          .pipe(source('bundle.js'))
+          .pipe(gulp.dest("dist"));
+      });
+      ```
+
+    3. 与`Watchify`结合
+
+      ```js
+      npm install --save-dev watchify gulp-util
+
+      var watchedBrowserify = watchify(browserify({
+          basedir: '.',
+          debug: true,
+          entries: ['src/main.ts'],
+          cache: {},
+          packageCache: {}
+      }).plugin(tsify));
+
+      function bundle() {
+          return watchedBrowserify
+              .bundle()
+              .pipe(source('bundle.js'))
+              .pipe(gulp.dest("dist"));
+      }
+
+      gulp.task("default", bundle);
+      watchedBrowserify.on("update", bundle);
+      watchedBrowserify.on("log", gutil.log);
+      ```
+
+    4. 使用`sourcemaps`     以js为例
+
+      ```js
+      npm install --save gulp-sourcemaps
+
+      gulp.task('sourcemap',function() {
+          gulp.src('./src/*.js')  
+          .pipe( sourceMap.init() ) //启用sourcemap功能
+          .pipe( concat('all.js') )  
+          .pipe( uglify() )  
+          .pipe( sourceMap.write('../maps/',{addComment: false}) )  //生成记录位置信息的sourcemaps文件
+          .pipe( gulp.dest('./dist/') )
+      });
+
+      //sourceMap.write( )，不传参，将会直接在 all.js 尾部，生成sourcemaps信息。
+      //sourceMap.write( path )，将会在指定的 path，生成独立的sourcemaps信息文件。如果指定的是相对路径，是相对于 all.js 的路径。
+      //无法指定路径为 src 目录，否则，sourcemaps文件会生成在 dist 目录下。
+      //addComment : true / false ; 是控制处理后的文件（本例是 all.js ），尾部是否显示关于sourcemaps信息的注释。
+      //不加这个属性，默认是true。设置为false的话，就是不显示。
+      ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,89 @@
 var gulp = require('gulp'),
-    del = require('del'),
-    oPath = require('path'),
-    less = require('gulp-less'),
-    cssmin = require('gulp-minify-css'),
-    babel = require("gulp-babel"),
-    uglify = require("gulp-uglify"),
-    sourcemaps = require("gulp-sourcemaps"),
-    connect = require('gulp-connect');
+		del = require('del'),
+		oPath = require('path'),
+		sass = require('gulp-sass'),
+		cssmin = require('gulp-minify-css'),
+		sourceMap = require('gulp-sourcemaps'),
+		uglify = require('gulp-uglify'),
+		ts = require("gulp-typescript"),
+		tsProject = ts.createProject("tsconfig.json"),
+		connect = require('gulp-connect');
 
 var env = 'dev'; // 用于执行gulp任务时的判断,默认为生产环境
-var paraTask = gulp.parallel(html, js, transLess); //并行执行任务序列
+var paraTask = gulp.parallel(html, images, transTs, transSass); //并行执行任务序列
 
-var jsPath = 'src/js/**/*.js',
-    cssPath = 'src/css/**/*.less',
-    htmlPath = 'src/*.html',
-    imgPath = 'src/assets/**/*.*';
-
+var jsPath = 'src/js/**/*.ts',
+		cssPath = 'src/css/**/*',
+		htmlPath = 'src/*.html',
+		imgPath = 'src/assets/**/*.*';
 
 //开发环境编译
-gulp.task('build', gulp.series(setEnv('build'), clean, paraTask), function(done) {
-  done();
+gulp.task('build', gulp.series(setEnv('build'), clean, paraTask), function (done) {
+	done();
 });
 
 //生产环境编译
-gulp.task('dev', gulp.series(setEnv('dev'), clean, paraTask), function(done) {
-  done();
+gulp.task('dev', gulp.series(setEnv('dev'), clean, paraTask), function (done) {
+	done();
 });
 
 //启动监听服务
-gulp.task('watch', function(done) {
-  //监听less或css
-  gulp.watch(cssPath, gulp.series(transLess, reload)).on('all', function(e, path, states) {
-    if (e === 'unlink') { //当删除文件时要做的事  相对应的：add:新增; change: 修改
-      del('dist/css/**/' + oPath.basename(path, '.less') + '.css');
-    }
-  });
-  gulp.watch(jsPath, gulp.series(js, reload)).on('all', function(e, path, states) {
-    if (e === 'unlink') {
-      del('dist/js/**/' + oPath.basename(path, '.js') + '.js');
-    }
-  });
-  gulp.watch(htmlPath, gulp.series(html, reload)).on('all', function(e, path, states) {
-    if (e === 'unlink') {
-      del('dist/' + oPath.basename(path, '.html') + '.html');
-    }
-  });
-  gulp.watch(imgPath, gulp.series(images, reload)).on('all', function(e, path, states) {
-    if (e === 'unlink') {
-      del('dist/assets/**/' + oPath.basename(path) );
-    }
-  });
-  return serve();
+gulp.task('watch', function (done) {
+	//监听less或css
+	gulp.watch(cssPath, gulp.series(transSass)).on('all', function (e, path, states) {
+		if (e === 'unlink') { //当删除文件时要做的事	相对应的：add:新增; change: 修改
+			del('dist/css/**/' + oPath.basename(path, '.scss') + '.css');
+		}
+	});
+	gulp.watch(jsPath, gulp.series(transTs)).on('all', function (e, path, states) {
+		if (e === 'unlink') {
+			del('dist/js/**/' + oPath.basename(path, '.ts') + '.js');
+		}
+	});
+	gulp.watch(htmlPath, gulp.series(html)).on('all', function (e, path, states) {
+		if (e === 'unlink') {
+			del('dist/' + oPath.basename(path, '.html') + '.html');
+		}
+	});
+	gulp.watch(imgPath, gulp.series(images, reload)).on('all', function (e, path, states) {
+		if (e === 'unlink') {
+			del('dist/assets/**/' + oPath.basename(path));
+		}
+	});
+	return serve();
 });
 
-//清除dist文件夹
-gulp.task('clean', function() {
-  return del(['dist/*']);
-});
-
-
-function setEnv(type){
-  return () => {
-    return new Promise(function(resolve, reject) {
-      console.log(type);
-      env = type || 'dev';
-      resolve();
-    })
-  }
+function setEnv(type) {
+	return () => {
+		return new Promise(function (resolve, reject) {
+			env = type || 'dev';
+			resolve();
+		});
+	}
 }
 
 function html() {
-  return gulp.src(htmlPath, {base: 'src'})
-    .pipe(gulp.dest('dist'));
+	return gulp.src(htmlPath, { base: 'src' })
+		.pipe(gulp.dest('dist'));
 }
 
 function images() {
-  return gulp.src(imgPath, {base: 'src'})
-    .pipe(gulp.dest('dist'));
+	return gulp.src(imgPath, { base: 'src' })
+		.pipe(gulp.dest('dist'));
 }
 
-//使用babel编译es
-function js() {
-  var stream = gulp.src(jsPath, {base: 'src'}).pipe(sourcemaps.init()).pipe(babel());
+//编译typescript
+function transTs() {
+	var stream = tsProject.src().pipe(sourceMap.init()).pipe(tsProject()).js;
   env === 'dev' ? stream.pipe(uglify({ mangle: true, compress: true })) : '';
-  return stream.pipe(sourcemaps.write(".")).pipe(gulp.dest("dist"));
+  return stream.pipe(sourceMap.write('../maps/',{addComment: false})).pipe(gulp.dest("dist/js"));
 }
 
-//编译less
-function transLess() {
-  var stream = gulp.src(cssPath, {base: 'src'}).pipe(less());
-  env === 'dev' ? stream.pipe(cssmin()) : '';
-  return stream.pipe(gulp.dest("dist"));
-}
-
-//清除dist文件
-function clean() {
-  return del('dist/*');
+//编译sass
+function transSass() {
+	var stream = gulp.src(cssPath, { base: 'src' }).pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError));
+	env === 'dev' ? stream.pipe(cssmin()) : '';
+	return stream.pipe(gulp.dest("dist"));
 }
 
 function reload() {
@@ -108,4 +96,9 @@ function serve() {
 		livereload: true,
 		port: 9090
   });
+}
+
+//清空dist文件夹
+function clean() {
+	return del('dist/*');
 }
